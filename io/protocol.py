2419e2404f4a44b750600b9f9b0a7f6b9c97839f
Dominik Jancik <hi@domj.net>
2021-12-18 13:11:34 +0200

    Refactor, add more test stubs.

--- io/protocol.py
+++ io/protocol.py
@@ -1,5 +1,11 @@
-def parse_message(message: str) -> dict:
+from typing import Mapping
+
+
+def tokenize_message(message: str) -> Mapping[str, str]:
     """
     Parse key-value pairs from a string message into a dict
     """
+    pass
+
+def parse_message_tokens(message_tokens: Mapping[str, str]):
     pass
--- io/protocol.test.py
+++ io/protocol.test.py
@@ -1,8 +1,21 @@
 import unittest
 
-class TestParseMessage(unittest.TestCase):
+class TestTokenizeMessage(unittest.TestCase):
     def test_should_parse_single_keyvalue_pair_in_line(self):
         pass
 
     def test_should_parse_multiple_keyvalue_pairs_per_line(self):
+        pass
+
+class TestParseMessageTokens(unittest.TestCase):
+    def test_should_parse_play_message_as_boolean(self):
+        pass
+
+    def test_should_parse_stop_message_as_boolean(self):
+        pass
+
+    def test_should_parse_row_message_as_array(self):
+        pass
+
+    def test_should_parse_tempo_message_as_int(self):
         pass
