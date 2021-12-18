e666dbbf07f33d6ca4a6e4c3f480642c358ba486
Dominik Jancik <hi@domj.net>
2021-12-18 13:01:55 +0200

    Add protocol.parse_message stubs including Tests

---
+++ io/protocol.py
@@ -0,0 +1,5 @@
+def parse_message(message: str) -> dict:
+    """
+    Parse key-value pairs from a string message into a dict
+    """
+    pass
---
+++ io/protocol.test.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestParseMessage(unittest.TestCase):
+    def test_should_parse_single_keyvalue_pair_in_line(self):
+        pass
+
+    def test_should_parse_multiple_keyvalue_pairs_per_line(self):
+        pass
